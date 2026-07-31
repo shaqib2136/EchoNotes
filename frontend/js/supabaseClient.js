43dcc75271430295e8cbef3bf996823a3120452f
Shaqib Ahamad <sagaciousshaqib@gmail.com>
2026-07-31 21:44:18 +0530

    update

--- frontend/js/supabaseClient.js
+++ frontend/js/supabaseClient.js
@@ -1,19 +1,12 @@
 console.log("supabaseClient.js loaded");
 
 const SUPABASE_URL = "https://uwhfjhhafozgobzamvfd.supabase.co";
-const SUPABASE_KEY = "sb_publishable_dMzMLsrAcQ3Ui5AiBZO1fw_Hve6ZDnV";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_dMzMLsrAcQ3Ui5AiBZO1fw_Hve6ZDnV";
 
-console.log("window.supabase =", window.supabase);
-
-const client = window.supabase.createClient(
+// Create ONE client and expose it globally
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_KEY
+  SUPABASE_PUBLISHABLE_KEY
 );
-
-console.log("client =", client);
-
-window.supabaseClient = client;
-
-
 
 
